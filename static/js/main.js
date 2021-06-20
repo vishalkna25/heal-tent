99ba3b22a944598b70182553485ac7b81dbad999
vishalkna25 <60284405+vishalkna25@users.noreply.github.com>
2021-06-20 22:19:02 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// landing page add display control
+let addControlIc = document.getElementById('haS-bt');
+let addContainer = document.getElementById('ha-S');
+addControlIc.addEventListener('click', ()=> {
+    addContainer.style.display = 'none';
+});
